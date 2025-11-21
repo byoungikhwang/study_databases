23f4095f763a39c4f8e3fe11daab58e575c0d021
saintjin0 <byoungikhwang@gmail.com>
2025-11-21 09:24:34 +0900

    select_froms.sql

--- sqls/select_froms.sql
+++ sqls/select_froms.sql
@@ -1,6 +1,6 @@
 --  SELECT personid, lastname, firstname, address, sity
 --  FROM table_name;
-
+ 
 -- SELECT PersonID, LastName, FirstName, Address, City
 -- FROM Persons;
 SELECT PersonID, LastName, FirstName, Address, City
